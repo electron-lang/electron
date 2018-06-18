d7d8246ad17f54a53fb384a97a00b654941897e5
David Craven <david@craven.ch>
2018-06-18 14:00:22 +0200

    Improve error messages.

--- electron/src/cli.ts
+++ electron/src/cli.ts
@@ -2,6 +2,7 @@
 import * as fs from 'fs';
 import * as path from 'path';
 import { Command } from 'commander';
+import chalk from 'chalk';
 import * as electron from './index';
 
 const program = new Command('electron')
@@ -12,9 +13,10 @@ program.command('compile <file>')
     .action((file) => {
         const text = fs.readFileSync(path.resolve(file)).toString()
         const result = electron.compile(text)
+        const lines = text.split('\n')
         if (result.errors.length > 0) {
             for (let error of result.errors) {
-                reportDiagnostic(error)
+                reportDiagnostic(file, lines[error.src.startLine - 1], error)
             }
         }
         if (result.ast) {
@@ -29,18 +31,26 @@ if (!process.argv.slice(2).length) {
     program.outputHelp()
 }
 
-function reportDiagnostic(diagnostic: electron.IDiagnostic) {
-    const line = diagnostic.src.startLine.toString()
-    const column = diagnostic.src.startColumn.toString()
-    const ty = diagnostic.errorType
-    let message = `[${ty}] ${diagnostic.message} (${line}, ${column})`
-    let print = console.info
-
-    if (diagnostic.severity === electron.DiagnosticSeverity.Error) {
-        print = console.error
-    } else if (diagnostic.severity === electron.DiagnosticSeverity.Warning) {
-        print = console.warn
+function reportDiagnostic(path: string, lineContents: string,
+                          diagnostic: electron.IDiagnostic) {
+    const file = chalk.magenta(path)
+    const lineNumber = diagnostic.src.startLine.toString()
+    {
+        const line = chalk.cyan(lineNumber)
+        const column = chalk.cyan(diagnostic.src.startColumn.toString())
+        let ty = ''
+        if (diagnostic.severity === electron.DiagnosticSeverity.Error) {
+            ty = chalk.red('error')
+        } else if (diagnostic.severity === electron.DiagnosticSeverity.Warning) {
+            ty = chalk.yellow('warn')
+        } else if (diagnostic.severity === electron.DiagnosticSeverity.Info) {
+            ty = chalk.blue('info')
+        }
+        const message = `${file}:${line}:${column} - ${ty}: ${diagnostic.message}\n`
+        console.error(message)
     }
-
-    print(message)
+    const line = chalk.black(chalk.bgWhite(lineNumber))
+    const indent = chalk.bgWhite(' '.repeat(lineNumber.length))
+    const lineMessage = `${line}\t${lineContents}\n${indent}\n\n`
+    console.error(lineMessage)
 }
