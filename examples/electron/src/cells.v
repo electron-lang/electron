637b8bda3adc61d701e51838ffb60ce51d1fa358
David Craven <david@craven.ch>
2018-08-13 19:25:49 +0200

    Update examples.

---
+++ examples/electron/src/cells.v
@@ -0,0 +1,27 @@
+`timescale 1ns/100ps
+
+`ifndef SYNTHESIS
+module Clock(output reg clk);
+   parameter PERIOD = 1;
+   parameter TIME = 100;
+   parameter VCD = "bench.vcd";
+
+   initial begin
+      clk = 0;
+   end
+
+   always begin
+      #(PERIOD) clk = ~clk;
+   end
+
+   initial begin
+      #(TIME);
+      $finish;
+   end
+
+   initial begin
+      $dumpfile(VCD);
+      $dumpvars;
+   end
+endmodule
+`endif
